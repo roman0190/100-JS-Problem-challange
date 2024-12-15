c2051e488b1d462bec907849e349af27c0bd8d3d
HOWLADAR, ROMAN <131019051+roman0190@users.noreply.github.com>
2024-12-15 21:21:10 +0600

    Palindrome

---
+++ Palindrome.js
@@ -0,0 +1,22 @@
+const x = 0;
+
+function Palindrome(x) {
+  if (x < 0) return false;
+  const val = Array.from(String(x), Number);
+  let count = 0;
+  let halflength = Math.round(val.length / 2);
+  for (let i = 0, j = val.length - 1; i <= j; i++, j--) {
+    if (val[i] === val[j]) {
+      count++;
+    }
+  }
+  if (count === halflength) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const result = Palindrome(x);
+
+console.log(result);
